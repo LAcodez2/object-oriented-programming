044e8bfae8c35ca3bb50f2fc3e35b8b2eff137de
Albert Valderas <albertvalderas052101@gmail.com>
2025-03-14 19:53:15 -0500

    added exercises

--- constructor-exercise.js
+++ constructor-exercise.js
@@ -1,1 +1,31 @@
-/* Please read the instructions located in the 1-constructor-exercise-readme.md file and complete your code below */ 
+/* Please read the instructions located in the 1-constructor-exercise-readme.md file and complete your code below */
+
+function Book(title, author, year, genre) {
+  this.title = title;
+  this.author = author;
+  this.year = year;
+  this.genre = genre;
+}
+
+Book.prototype.getSummary = function () {
+  return `${this.title} (${this.year}) - ${this.genre}`;
+};
+
+Book.prototype.isClassic = function () {
+  Book.prototype.isClassic = function () {
+    const currentYear = new Date().getFullYear();
+    return currentYear - this.year > 50;
+  };
+};
+
+Book.prototype.updateYear = function (newYear) {
+  this.year = newYear;
+  return `Publication year updated to ${newYear}`;
+};
+
+Book.prototype.changeGenre = function (newGenre) {
+  this.genre = newGenre;
+  return `Genre updated to ${newGenre}`;
+};
+
+const book1 = new Book("To Kill a Mockingbird", "Harper Lee", 1960, "Fiction");
--- oop-exercise.js
+++ oop-exercise.js
@@ -1,1 +1,77 @@
 /* Please read the instructions located in the exercises/2-oop-exercise-readme.md file and complete your code below */
+
+class Student {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    this.grades = [];
+  }
+
+  addGrade(grade) {
+    if (typeof grade === "number" && grade >= 0 && grade <= 100) {
+      this.grades.push(grade);
+    } else {
+      console.log("Grade must be between 0 and 100.");
+    }
+  }
+
+  calculateAverageGrade() {
+    if (this.grades.length === 0) return 0;
+    const sum = this.grades.reduce((total, grade) => total + grade, 0);
+    return sum / this.grades.length;
+  }
+
+  hasPassed(passingGrade) {
+    return this.calculateAverageGrade >= passingGrade;
+  }
+
+  getName() {
+    return this.name;
+  }
+
+  getAge() {
+    return this.age;
+  }
+}
+
+class Course {
+  constructor(title) {
+    this.title = title;
+    this.students = [];
+  }
+
+  enrollStudent(student) {
+    if (student instanceof Student) {
+      this.students.push(student);
+    } else {
+      console.log("Invalid student.");
+    }
+  }
+
+  listStudents() {
+    if (this.students.length === 0) {
+      console.log(`No students are enrolled in ${this.title}`);
+    } else {
+      console.log(`Students enrolled in ${this.title}`);
+      this.students.forEach((student, index) => {
+        console.log(
+          `${index + 1}. ${student.getName()} (Age: ${student.getAge()})`
+        );
+      });
+    }
+  }
+
+  calculateCourseAverage() {
+    if (this.students.length === 0) return 0;
+    const totalAverage = this.students.reduce((sum, student) => {
+      return sum + student.calculateAverage();
+    }, 0);
+
+    return totalAverage / this.students.length;
+  }
+}
+
+const student1 = new Student('Alice', 20);
+student1.addGrade(85);
+student1.addGrade(90);
+student1.addGrade(78);
